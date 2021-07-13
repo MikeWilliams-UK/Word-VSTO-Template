bd2d062cde6701a0443e141217ccbefc4a207a54
Mike Williams <mike.williams@tba.group>
2021-07-13 11:09:05 +0100

    First attempt to try to set _AssemblyLocation

---
+++ PostBuild/FullyQualifyAssemblyLocation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml.Packaging;
+
+namespace FullyQualifyAssemblyLocation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WordprocessingDocument template = WordprocessingDocument.Open(args[0], true))
+            {
+                var customPropertiesPart = template.CustomFilePropertiesPart;
+                var props = template.CustomFilePropertiesPart.Properties;
+                var prop = props.FirstChild;
+
+                var parts = prop.InnerText.Split('|');
+                parts[0] = args[0];
+                //prop.InnerText = string.Join('|', parts);
+                
+                Debugger.Break();
+            }
+        }
+    }
+}
